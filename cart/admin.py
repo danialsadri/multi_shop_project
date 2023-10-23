b979fa9b81f9e1dce47ad542668629017ad2258e
danial sadri <danielsadri01@gmail.com>
2023-10-23 11:57:32 +0330

    add order and oderitem  model

--- cart/admin.py
+++ cart/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+    extra = 0
+    classes = ['collapse']
+    show_change_link = True
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_price', 'is_paid', 'created']
+    list_filter = ['is_paid', 'created']
+    search_fields = ['user']
+    raw_id_fields = ['user']
+    inlines = [OrderItemInline]
--- cart/models.py
+++ cart/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from accounts.models import User
+from product.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    total_price = models.PositiveBigIntegerField(default=0)
+    is_paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.phone_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    size = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    quantity = models.PositiveBigIntegerField()
+    price = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.order.user.phone_number
