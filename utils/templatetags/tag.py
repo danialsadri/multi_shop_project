d635103dc2b0a749d2d647e4bfe8737a895f93bb
danial sadri <danielsadri01@gmail.com>
2023-10-25 11:21:54 +0330

    add template tag

--- utils/templatetags/tag.py
+++ utils/templatetags/tag.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from product.models import Product
 
 register = template.Library()
@@ -8,3 +10,8 @@ register = template.Library()
 def resent_products(count=1):
     products = Product.objects.order_by('-created')[:count]
     return {'products': products}
+
+
+@register.simple_tag()
+def popular_products(count=1):
+    return Product.objects.annotate(comments_count=Count('comments')).order_by('-comments_count')[:count]
