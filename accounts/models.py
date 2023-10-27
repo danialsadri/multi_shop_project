b6e06fe4cc200b9bcf844ac0ce382ab8087be2ba
danial sadri <danielsadri01@gmail.com>
2023-10-27 12:17:54 +0330

    translate بخ models  page

--- accounts/models.py
+++ accounts/models.py
@@ -17,8 +17,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = _('کاربر')
-        verbose_name_plural = _('کاربر ها')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def __str__(self):
         return self.phone_number
--- cart/models.py
+++ cart/models.py
@@ -1,35 +1,48 @@
 from django.db import models
 from accounts.models import User
 from product.models import Product
+from django.utils.translation import gettext_lazy as _
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    total_price = models.PositiveBigIntegerField(default=0)
-    address = models.TextField(max_length=500, blank=True, null=True)
-    is_paid = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders', verbose_name=_('user'))
+    total_price = models.PositiveBigIntegerField(default=0, verbose_name=_('total_price'))
+    address = models.TextField(max_length=500, blank=True, null=True, verbose_name=_('address'))
+    is_paid = models.BooleanField(default=False, verbose_name=_('is_paid'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
 
     def __str__(self):
         return self.user.phone_number
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
-    size = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
-    quantity = models.PositiveBigIntegerField()
-    price = models.PositiveBigIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name=_('order'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items', verbose_name=_('product'))
+    size = models.CharField(max_length=100, verbose_name=_('size'))
+    color = models.CharField(max_length=100, verbose_name=_('color'))
+    quantity = models.PositiveBigIntegerField(verbose_name=_('quantity'))
+    price = models.PositiveBigIntegerField(verbose_name=_('price'))
+
+    class Meta:
+        verbose_name = _('OrderItems')
+        verbose_name_plural = _('OrderItems')
 
     def __str__(self):
         return self.order.user.phone_number
 
 
 class DiscountCode(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    discount = models.PositiveSmallIntegerField(default=0)
-    quantity = models.PositiveSmallIntegerField(default=1)
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('name'))
+    discount = models.PositiveSmallIntegerField(default=0, verbose_name=_('discount'))
+    quantity = models.PositiveSmallIntegerField(default=1, verbose_name=_('quantity'))
+
+    class Meta:
+        verbose_name = _('DiscountCode')
+        verbose_name_plural = _('DiscountCodes')
 
     def __str__(self):
         return self.name
