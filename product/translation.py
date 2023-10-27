4992a581f8b9b4bbdc947b5c967392779d4f4c98
danial sadri <danielsadri01@gmail.com>
2023-10-27 15:23:43 +0330

    deleted category translation

--- product/translation.py
+++ product/translation.py
@@ -2,11 +2,6 @@ from modeltranslation.translator import TranslationOptions, register
 from .models import *
 
 
-@register(Category)
-class CategoryTranslationOptions(TranslationOptions):
-    fields = ['title']
-
-
 @register(Color)
 class ColorTranslationOptions(TranslationOptions):
     fields = ['title']
